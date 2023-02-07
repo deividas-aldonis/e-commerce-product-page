f4db5691fc7e7497808f5b76fd4a8f3513b3b6e5
Deividas <deividas.aldonis.dev@gmail.com>
2023-02-07 12:41:53 +0200

    add mobile menu

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const shoppingCart = document.querySelector(".shopping-cart");
 const shoppingCartBtn = document.querySelector(".shopping-cart-btn");
 
+const menuOverlay = document.querySelector(".menu-overlay");
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+
 shoppingCartBtn.addEventListener("click", () => {
   shoppingCart.classList.toggle("hide");
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  menuOverlay.classList.add("hide");
+  menu.classList.add("hide");
+});
+
+openMenuBtn.addEventListener("click", () => {
+  menuOverlay.classList.remove("hide");
+  menu.classList.remove("hide");
 });
